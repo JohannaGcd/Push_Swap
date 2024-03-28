5054f78727c23ac3aaddbd3c4a86059ab49f2bf7
JoGcd <jguacide@f1r2s20.codam.nl>
2024-03-28 11:27:28 +0100

    checked by peer. one modif: returns 0 if empty argument. no error message

--- main.c
+++ main.c
@@ -1,32 +1,30 @@
 #include "push_swap.h"
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_list *stack_a;
-	t_list *stack_b;
+	t_list	*stack_a;
+	t_list	*stack_b;
+	int		i;
 
 	stack_a = NULL;
 	stack_b = NULL;
-
 	if ((argc == 1) || (argc == 2 && !argv[1][0]))
 	{
-		write(STDERR_FILENO, "Error\n", 6);
-		//free_errors(&stack_a);
-		//free_errors(&stack_b);
-		return (1);
-
+		// write(STDERR_FILENO, "Error\n", 6);
+		// free_errors(&stack_a);
+		// free_errors(&stack_b);
+		return (0);
 	}
 	else if (argc == 2)
 	{
-		argv = ft_split(argv[1], ' '); 
+		argv = ft_split(argv[1], ' ');
 		stack_init(&stack_a, argv);
 	}
 	else
 		stack_init(&stack_a, argv + 1);
-
 	if (!stack_sorted(stack_a))
-	{	
+	{
 		if (ft_lstsize(stack_a) == 2)
 			sa(&stack_a, true);
 		else if (ft_lstsize(stack_a) == 3)
@@ -36,20 +34,19 @@ int main(int argc, char **argv)
 		else if (ft_lstsize(stack_a) == 5)
 			sort_five(&stack_a, &stack_b);
 		else
-			big_sort(&stack_a, &stack_b);	
+			big_sort(&stack_a, &stack_b);
 	}
-
 	if (argc == 2)
 	{
-		int i = 0;
+		i = 0;
 		while (argv[i])
 		{
 			free(argv[i]);
 			i++;
-		}	
+		}
 		free(argv);
 	}
-	ft_lstclear(&stack_a);	
+	ft_lstclear(&stack_a);
 	ft_lstclear(&stack_b); // clear only stack_a?
 	return (0);
 }
